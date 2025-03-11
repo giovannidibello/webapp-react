26c988693e908fa9e71769a7a1891eadd3a8e9fa
Giovanni <dibellogio@gmail.com>
2025-03-11 15:50:17 +0100

    add Review Card component

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,28 @@
+// ReviewCard.jsx
+
+const ReviewCard = () => {
+
+    return (
+
+        <div className="card mb-4">
+
+            <div className="card-body">
+
+                <p className="card-text">A mind-bending masterpiece.</p>
+
+                <strong>Vote: 5 </strong>
+
+                <address><i>By Alice</i></address>
+            </div>
+
+        </div>
+
+    )
+
+}
+
+
+
+
+
+export default ReviewCard
