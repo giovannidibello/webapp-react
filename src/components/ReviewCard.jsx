ef3e7994fff6ac7359e5012c678a47b59bc863af
Giovanni <dibellogio@gmail.com>
2025-03-12 15:59:56 +0100

    add stars for vote review

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,8 +1,40 @@
 // ReviewCard.jsx
 
+// importo fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
 const ReviewCard = (props) => {
 
     const { name, vote, text } = props.reviewProp;
+
+    // funzione per mostrare le stelle in base al voto
+    const renderStars = (vote) => {
+
+        let stars = [];
+
+        for (let i = 0; i < 5; i++) {
+            if (i < vote) {
+                stars.push(
+                    <FontAwesomeIcon
+                        key={i}
+                        icon={faStar}
+                        style={{ color: "#FFD43B" }}
+                    />
+                );
+            } else {
+                stars.push(
+                    <FontAwesomeIcon
+                        key={i}
+                        icon={faStar}
+                        style={{ color: "#ccc" }}
+                    />
+                );
+            }
+        }
+
+        return stars;
+    };
 
     return (
 
@@ -12,7 +44,7 @@ const ReviewCard = (props) => {
 
                 <p className="card-text">{text || "Text not found"}</p>
 
-                <strong>Vote: {vote} </strong>
+                <strong>Vote: {renderStars(vote)} </strong>
 
                 <address><i>By {name} </i></address>
             </div>
